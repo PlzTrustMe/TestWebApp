9a96b1ba74e4a4c7e8acbe37ffc9b3b3e449564f
Try <try@gmail.com>
2023-11-22 00:27:48 +0200

    Create base desc component

---
+++ front/src/shared/BaseDescription.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-col gap-2.5 text-xs text-tg-theme-hint mt-2.5 pl-2.5">
+    {{ props.text }}
+
+    <slot name="content"></slot>
+  </div>
+</template>
+
