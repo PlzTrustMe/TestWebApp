eb0c2f941924be2118dec0c175b7509d30a493b1
Try <try@gmail.com>
2023-11-25 09:19:47 +0200

    feature/add switcher component

---
+++ front/src/shared/BaseToggle.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {useSettingsStore} from "@/states.js";
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+
+  isToggle: {
+    type: Boolean,
+    required: true,
+  }
+})
+
+const settingsStore = useSettingsStore()
+</script>
+
+<template>
+  <div class="flex justify-between items-center p-3 bg-tg-theme-secondary-bg rounded-lg">
+    <div class="">{{ props.text }}</div>
+    <span @click="settingsStore.setUserAutoTranscription()"
+          :class="{'bg-tg-theme-accent before:translate-x-[26px] transition-transform duration-300': isToggle}"
+          class="cursor-pointer bg-tg-theme-bg w-[50px] h-[30px] px-0.5 flex items-center
+          rounded-[30px] before:content-[''] before:inline-block before:rounded-[50%] before:w-[24px] before:h-[24px]
+          before:bg-tg-theme-text before:transition-transform before:duration-300">
+    </span>
+  </div>
+</template>
+
